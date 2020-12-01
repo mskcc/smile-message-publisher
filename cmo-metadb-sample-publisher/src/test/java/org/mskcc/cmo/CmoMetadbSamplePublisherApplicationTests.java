8bd0b7331dd06bfe5002d388a18c3142440db478
laptop <laptop@lski3071.myfiosgateway.com>
2020-12-01 14:26:30 -0500

    Edited white spaces

---
+++ cmo-metadb-sample-publisher/src/test/java/org/mskcc/cmo/CmoMetadbSamplePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mskcc.cmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmoMetadbSamplePublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
